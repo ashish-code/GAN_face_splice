7ebd839823245001a73f04df0c5912792f618656
hurlbuttm <michael_hurlbutt@partech.com>
2018-05-11 11:17:02 -0400

    Plugin Clarifications

--- __init__.py
+++ __init__.py
@@ -3,11 +3,14 @@ from maskgen.plugins import findPlugin
 import os
 
 """JT code only used for plugin conversion"""
+
+
 def transform(img, source, target, **kwargs):
 	donor = kwargs['donor']
-	pGan_fSplice.loadModel(os.path.join(findPlugin('GAN_face_splice'),pGan_fSplice.PREDICTOR_PATH))
-	pGan_fSplice.splice_donor_recipient(source,donor,target)
-	return None, None
+	pGan_fSplice.loadModel(os.path.join(findPlugin('GAN_face_splice'), pGan_fSplice.PREDICTOR_PATH))
+	pGan_fSplice.splice_donor_recipient(source, donor, target)
+	return {'subject': 'face', 'purpose': 'add'}, None
+
 
 def operation():
 	return {
@@ -15,14 +18,53 @@ def operation():
 		'category': 'Paste',
 		'software': 'GAN_face_splice',
 		'version': '0.1',
-		'arguments':{
-			'donor':{
+		'arguments': {
+			'donor': {
 				'type': 'donor',
 				'description': 'The donor image from which to select the face'
+			},
+			'donor rotated': {
+				'type': 'yesno',
+				'description': 'Enter yes if the donor is rotated during the paste operation',
+				'default value': 'yes'
+			},
+			'donor cropped': {
+				'type': 'yesno',
+				'description': 'Enter yes if the donor is cropped during the paste operation. '
+							   'Ideally, crop should occur as a SelectRegion operation just prior to donation',
+				'default value': 'no'
+			},
+			'donor resized': {
+				'type': 'yesno',
+				'description': 'Enter yes if the donor is resized during the paste operation',
+				'default value': 'yes'
+			},
+			'purpose': {
+				'type': 'list',
+				'values': [
+					'remove',
+					'add',
+					'blend'
+				],
+				'description': 'Purpose: remove an object, add an object.',
+				'default value': 'add'
+			},
+			'subject': {
+				'type': 'list',
+				'values': [
+					'people',
+					'face',
+					'natural object',
+					'man-made object',
+					'large man-made object',
+					'landscape',
+					'other'
+				],
+				'default value': 'face'
 			}
 		},
-		'description':'Swap the faces of two photographs',
-		'transitions':[
+		'description': 'Swap the faces of two photographs',
+		'transitions': [
 			'image.image'
 		]
 	}
